81bf93c77b2e73b2a8008d68615c1c07c85d88e0
Gamalina <danielrudcallesen96@gmail.com>
2022-09-05 13:36:04 +0200

    Første commit

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstGitHubClassRoomConsoleApp60
+{
+    internal class Demo
+    {
+        public int _id;
+        public string _name;
+        public string _title;
+        public string _description;
+        public string _url;
+
+        public int Id
+        {
+            get => _id;
+            set => _id = value;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Title
+        {
+            get => _title;
+            set => _title = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Description
+        {
+            get => _description;
+            set => _description = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public string Url
+        {
+            get => _url;
+            set => _url = value ?? throw new ArgumentNullException(nameof(value));
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Id)}: {Id}, {nameof(Name)}: {Name}, {nameof(Title)}: {Title}, {nameof(Description)}: {Description}, {nameof(Url)}: {Url}";
+        }
+    }
+}
